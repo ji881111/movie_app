9123e7bfdbe34c4bd913b20b2337b007e2e6a4a9
ji881111 <ji881111@naver.com>
2019-02-17 23:43:58 +0900

    Updates

---
+++ precache-manifest.ad61ffff3c1201c704a1e81202b0ceb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "37143e0fc8345b7037d2",
+    "url": "/movie_app/static/js/main.37143e0f.chunk.js"
+  },
+  {
+    "revision": "8ace2eaf1c51223fac7b",
+    "url": "/movie_app/static/js/2.8ace2eaf.chunk.js"
+  },
+  {
+    "revision": "37143e0fc8345b7037d2",
+    "url": "/movie_app/static/css/main.61093bf4.chunk.css"
+  },
+  {
+    "revision": "cba7b3298d198ba8632764cb1d2eb67d",
+    "url": "/movie_app/index.html"
+  }
+];
